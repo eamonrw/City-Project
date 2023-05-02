8fcc4bc95af9424f3f68257b80ea69fc17ef6dfb
Eamon Weingold <eamonweingold@gmail.com>
2023-05-01 23:29:26 -0400

    Initial commit

---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+    public Camera sceneCamera;
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+    private float step;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = sceneCamera.transform.position + sceneCamera.transform.forward * 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = 5.0f * Time.deltaTime;
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.0f) centerCube();
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft)) transform.Rotate(0, 5.0f * step, 0);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight)) transform.Rotate(0, -5.0f * step, 0);
+        if (OVRInput.GetUp(OVRInput.Button.One))
+        {
+            OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+        }
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0.0f)
+        {
+            transform.position = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+            transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        }
+    }
+
+    void centerCube()
+    {
+        targetPosition = sceneCamera.transform.position + sceneCamera.transform.forward * 3.0f;
+        targetRotation = Quaternion.LookRotation(transform.position - sceneCamera.transform.position);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, step);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, step);
+    }
+}
